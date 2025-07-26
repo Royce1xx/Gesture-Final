cd4dc71cc238c41cbe1ba376a210c2ddd4d0f2dc
Eugene1x <roycekinchloe@gmail.com>
2025-07-26 15:47:34 -0400

    Using openCV funcs i drew around my hand. Nothing crazy tbh

--- handtracker.py
+++ handtracker.py
@@ -1,39 +1,35 @@
 import cv2
 import numpy as np
 
-
-
-
 cv2.namedWindow("Hand Tracking", cv2.WINDOW_NORMAL)
-
 cap = cv2.VideoCapture(0)
-
 
 lowerSkin = np.array([0, 133, 77], dtype=np.uint8)
 upperSkin = np.array([255, 173, 127], dtype=np.uint8)
 
-
-
-
 while True:
     ret, frame = cap.read()
-
     if not ret:
         break
 
     frame = cv2.flip(frame, 1)
-
     colorConvert = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)
-
     skinMask = cv2.inRange(colorConvert, lowerSkin, upperSkin)
-
-
     skinMask = cv2.GaussianBlur(skinMask, (5, 5), 0)
 
+    cont, _ = cv2.findContours(skinMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if cont:
+        handCont = max(cont, key=cv2.contourArea)
+
+        if cv2.contourArea(handCont) > 1000:
+            cv2.drawContours(frame, [handCont], -1, (0, 255, 0), 2)
 
     cv2.imshow("Hand Tracker", frame)
     cv2.imshow("Skin Mask", skinMask)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break   
+        break
 
+cap.release()
+cv2.destroyAllWindows()
